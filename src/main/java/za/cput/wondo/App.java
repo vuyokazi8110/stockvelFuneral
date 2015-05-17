1a06831d7abb8d41c57ff820f5926fa0e97d8345
sbm <sbm@155.238.112.169>
2015-05-17 02:23:48 +0200

    updated services

--- src/main/java/za/cput/wondo/App.java
+++ src/main/java/za/cput/wondo/App.java
@@ -6,10 +6,25 @@
 
 package za.cput.wondo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 /**
  *
  * @author sbm
  */
-public class App {
-    
+@Configuration
+@ComponentScan
+@EnableWebMvc
+@EnableAutoConfiguration
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class,args);
+    }
 }
--- src/test/java/za/cput/wondo/service/MemberNameServicesTest.java
+++ src/test/java/za/cput/wondo/service/MemberNameServicesTest.java
@@ -6,17 +6,39 @@
 
 package za.cput.wondo.service;
 
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.Assert;
 import static org.testng.Assert.*;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import za.cput.wondo.domain.Policies;
+import za.cput.wondo.repository.PoliciesRepository;
 
 /**
  *
  * @author sbm
  */
-public class MemberNameServicesTest {
+public class MemberNameServicesTestextends AbstractTestNGSpringContextTests{
     
-    public MemberNameServicesTest() {
+    private Long id;
+    @Autowired
+    private PoliciesRepository repository;
+    @Test
+    public void create() throws Exception {
+        Map<int, int> values = new HashMap<int, int>();
+        values.put("policynumber",1011);
+        values.put("policyName","Accidental");
+        Policies policies = policiesFactory
+                .createPolicies(values,2);
+        
+        repository.save(policies);
+        PolicyNo=policies.getPolicyNo();
+        Assert.assertNotNull(policies);
+    }
     }
 
     // TODO add test methods here.
